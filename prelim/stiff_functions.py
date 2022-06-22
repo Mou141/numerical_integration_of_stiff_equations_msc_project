cea8b57650ebd8d225e958ff67357dbf46d888ad
= <17607121+Mou141@users.noreply.github.com>
2022-06-22 21:13:14 +0100

    Updated stiff_functions.py
    Added a tuple which encapsulates the ODE, analytical solution, and initial value of the stiff IVP already implemented in the file.

--- prelim/stiff_functions.py
+++ prelim/stiff_functions.py
@@ -1,6 +1,7 @@
 # Contains stiff problems for testing purposes (currently for preliminary investigation, may be incorporated into final package)
 # Requires numpy
-import numpy as np
+import numpy as np # For np.exp function
+from collections import namedtuple
 
 # There is an initial value problem which is stiff and also has an analytical solution: y'(t) = -15y(t), t >= 0, y(0) = 1. The solution being y(t) = exp(-15t)
 
@@ -20,3 +21,9 @@ def initial_value_solution(t):
         returns: exp(-15t)"""
     
     return np.exp(-15.0 * t)
+
+# Named tuple to contain IVP problem, solution, and initial value
+IVPTuple = namedtuple("IVPTuple", ["ODEFunction", "SolutionFunction", "y0"])
+
+# Named tuple that contains the problem, solution, and initial value for the stiff IVP implemented above
+STIFF_IVP = IVPTuple(ODEFunction=initial_value_problem, SolutionFunction=initial_value_solution, y0=0.0)
