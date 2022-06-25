8b79bb881999525a1c41f9b4514dacc239f916b9
= <17607121+Mou141@users.noreply.github.com>
2022-06-25 19:37:17 +0100

    Fixed error in calc_error.py
    Fixed import statement for test_ivp

--- prelim/calc_error.py
+++ prelim/calc_error.py
@@ -1,7 +1,7 @@
 # Calculates the fractional error for numerical solutions to initial value problems at each point of the solution
 import matplotlib.pyplot as plt # For plotting graphs
 import numpy as np # For maths functions and arrays
-from import test_ivp # For test_integrators function and INTEGRATORS global variable
+import test_ivp # For test_integrators function and INTEGRATORS global variable
 import stiff_functions # For STIFF_IVP and STIFF_IVP2
 
 def absolute_error(y_num, y_exact):
