a6668b87bff088746e2a0b430660cd7ca321fabe
= <17607121+Mou141@users.noreply.github.com>
2022-06-25 17:53:02 +0100

    Added find_integration_errors and get_error_by_dimension to calc_error.py
    Added code to find the error in an IVP for each solution, split the errors up by dimension, and plot them on separate graphs for each dimension.

--- prelim/calc_error.py
+++ prelim/calc_error.py
@@ -11,3 +11,54 @@ def absolute_error(y_num, y_exact):
 def fractional_error(y_num, y_exact):
     """Calculates the fractional error in a numerical solution by comparing with the exact solution."""
     return absolute_error(y_num, y_exact)/y_num
+
+def get_error_by_dimension(y_num, y_exact, ndim):
+    """Takes a numerical solution, an analytical solution, and the number of dimensions of the Initial Value Problem, and returns the fractional error listed by dimension."""
+    err = fractional_error(y_num, y_exact) # Calculate the fractional error
+    
+    if ndim == 1: # If ivp is only 1D...
+        return [err] # Return a list that contains only err
+        
+    else: # If ivp has more than 1 dimension...
+        return [err[i] for i in range(0, ndim)] # Split err into individual dimensions
+
+def find_integration_errors(results, ivp):
+    """Takes a dictionary that maps method names to results from scipy.integrate.solve_ivp and an Initial Value Problem, and plots the relative errors in that solution.
+        
+        Arguments:
+            results: Dictionary of results from scipy.integrate.solve_ivp
+            ivp: A tuple that contains the same information as stiff_functions.IVPTuple, in the same order."""
+    
+    ivp = stiff_functions.IVPTuple._make(ivp) # If ivp isn't an IVPTuple, convert it to one
+    methods = list(results.keys()) # Get the keys in the dictionary and put them into a list (need to do this as need to keep order same for iteration AND legend labelling)
+    
+    figure, ax = plt.subplots(ivp.ndim, 1, sharex=True) # Create a plot for each dimension of the IVP with a shared x-axis
+    
+    # Label the x-axis
+    ax[0].set_xlabel("t")
+    
+    if ivp.ndim == 1: # If ivp is only 1 dimensional...
+        ax[0].set_xlabel("err(y)")
+        
+    else: # If ivp has more than 1 dimension...
+        for i in range(0, ivp.ndim): # For each dimension...
+            ax[i].set_xlabel("err(y{0})".format((i+1)))
+    
+    for method, solution in zip(methods, map(results.get, methods)): # For each method in the list, get the method and the solution...
+        if solution.success: # If integration completed successfully for this method...
+            print("Integration succeeded for method '{0}'.".format(method))
+            
+            y_exact = ivp.SolutionFunction(solution.t) # Calculate the analytical solution for the t values of the numerical solution
+            err = get_error_by_dimension(solution.y, y_exact, ivp.ndim) # Get the error in the numerical solution by dimension
+            
+            for i in range(0, ivp.ndim): # For each dimension of the IVP...
+                ax[i].plot(solution.t, err[i]) # Plot the error in that dimension
+            
+        else: # If integration failed...
+            print("Integration failed for method '{0}'.".format(method))
+            
+            for i in range(0, ivp.ndim): # For each dimension of the IVP...
+                ax[i].plot([],[]) # Plot an empty graph (to keep legend ordering correct)
+    
+    figure.legend(methods, loc="upper right") # Add the methods to the legend and place the legend in the upper right
+    plt.show() # Display the graphs
