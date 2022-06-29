b2154745479eade98abbb07bb0d686ff5e0cd6e7
= <17607121+Mou141@users.noreply.github.com>
2022-06-29 18:31:51 +0100

    Parametrized ErrorTests.test_absolute_error
    Added test values to use with test_absolute_error

--- prelim/test/test_calc_error.py
+++ prelim/test/test_calc_error.py
@@ -6,9 +6,12 @@ sys.path.append("../") # Add the parent directory to the python path (since this
 import pytest
 import calc_error
 
+import numpy as np
+
 class ErrorTests:
     """Tests for absolute and relative errror calculations."""
     
+    @pytest.mark.parametrize("y_num,y_exact,a_err", [(1.0, 1.0, 0.0), (1.0, -1.0, 0.0), (np.array([1.0, 1.0, 0.5]), np.array([1.0, -1.0, 2.5]), np.array([0.0, 0.0, 2.0]))]) # Test values and outputs to use (test one value that would be the same with and without abs, one that needs abs, and one array to check it works properly with arrays)
     def test_absolute_error(y_num, y_exact, a_err):
         """Tests calc_error.absolute_error by asserting that calc_error.absolute_error(y_num, y_exact) === a_err."""
         assert calc_error.absolute_error(y_num, y_exact) == a_err
