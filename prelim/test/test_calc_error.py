33607f7471b992243175bc8f382cf45a84a7db9e
= <17607121+Mou141@users.noreply.github.com>
2022-06-29 19:42:44 +0100

    Added pytest.approx to Floating Point Comparisons

--- prelim/test/test_calc_error.py
+++ prelim/test/test_calc_error.py
@@ -15,9 +15,9 @@ class ErrorTests:
     @pytest.mark.parametrize("y_num,y_exact,a_err", [(1.0, 1.0, 0.0), (1.0, -1.0, 0.0), (np.array([1.0, 1.0, 0.5]), np.array([1.0, -1.0, 2.5]), np.array([0.0, 0.0, 2.0]))]) # Test values and outputs to use (test one value that would be the same with and without abs, one that needs abs, and one array to check it works properly with arrays)
     def test_absolute_error(y_num, y_exact, a_err):
         """Tests calc_error.absolute_error by asserting that calc_error.absolute_error(y_num, y_exact) == a_err."""
-        assert calc_error.absolute_error(y_num, y_exact) == a_err
+        assert calc_error.absolute_error(y_num, y_exact) == pytest.approx(a_err) # pytest.approx copes with floating point arithmetic issues
     
     @pytest.mark.parametrize("y_num,a_err,f_err", [(5.0, 2.0, (2.0/5.0)), (-5.0, 2.0, (2.0/5.0)), (np.array([5.0, -5.0, 0.5]), np.array([2.0, 2.0, 0.1]), np.array([(2.0/5.0), (2.0/5.0)], (1.0/5.0)))])
     def test_fractional_error(y_num, a_err, f_err):
         """Tests calc_error.fractional_error by asserting that calc_error.fractional_error(a_err, y_num) == f_err."""
-        assert calc_error.fractional_error(a_err, y_num) == f_err
+        assert calc_error.fractional_error(a_err, y_num) == pytest.approx(f_err) # pytest.approx copes with floating point arithmetic issues
