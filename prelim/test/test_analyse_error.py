9d0869313eb042df92fcf70bacacb9eb9c2855f5
= <17607121+Mou141@users.noreply.github.com>
2022-06-30 19:48:14 +0100

    Changed Comparison Method in test_calc_err_stats

--- prelim/test/test_analyse_error.py
+++ prelim/test/test_analyse_error.py
@@ -11,6 +11,19 @@ import numpy as np # For maths and arrays
 
 import analyse_error # Module to test
 
+def containers_are_equal(i1, i2):
+    """Compares two containers with pytest.approx and returns True if every element in i1 matches its equivalent in i2."""
+
+    if len(i1) != len(i2): # If the two containers don't have the same length...
+        return False
+
+    for v1, v2 in zip(i1, i2): # Iterate over i1 and i2 in parallel...
+        if v1 != pytest.approx(v2): # If the values don't match...
+            return False
+    
+    # If all comparisons succeeded...
+    return True
+    
 class TestStatsFunctions:
     """Tests the functions which calculate statistics (i.e. l^2 norm, l^infinity norm, and mean)."""
     
@@ -47,4 +60,4 @@ class TestStatsFunctions:
     @pytest.mark.parametrize("arr,stats", [stats_1, stats_2])
     def test_calc_err_stats(self, arr, stats):
         """Tests that analyse_error.test_calc_err_stats matches the contents of "stats"."""
-        assert analyse_error.calc_err_stats(arr) == pytest.approx(stats)
+        assert containers_are_equal(analyse_error.calc_err_stats(arr), stats)
