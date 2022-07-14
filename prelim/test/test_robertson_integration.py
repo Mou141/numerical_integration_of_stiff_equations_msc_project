af9bce3bd60d32bebf564ef1efc52f56c731430a
= <17607121+Mou141@users.noreply.github.com>
2022-07-14 23:04:08 +0100

    Added Test Suite for robertson_integration.py

---
+++ prelim/test/test_robertson_integration.py
@@ -0,0 +1,46 @@
+"""Contains tests for ../robertson_integration.py for use with pytest."""
+from pathlib import Path
+import sys
+
+# Add the parent directory of this file's directory to the python path (because robertson_ivp.py is in that directory)
+sys.path.append(str(Path(__file__).resolve().parent.parent))
+
+import robertson_ivp
+import robertson_integration
+
+import pytest
+
+
+class CmdArgsTest:
+    """Test parsing of command line arguments by robertson_integration.parse_args."""
+
+    # Test with both long and short option
+    @pytest.mark.parametrize("args", [["-us"], ["--use-static"]])
+    def test_parse_args_static(self, args):
+        """Tests that the specified command line arguments cause robertson_integration.parse_args to return robertson_ivp.TEST_IVP."""
+
+        assert robertson_integration.parse_args(args) is robertson_ivp.TEST_IVP
+
+    def test_parse_args_empty(self):
+        """Tests that empty command line arguments cause robertson_integration.parse_args to return a random set of initial values (i.e. that are not equal to those from robertson_ivp.TEST_IVP) but that the other members of the tuple are the same."""
+
+        ivp = robertson_integration.parse_args([])
+
+        assert ivp.y0 != robertson_ivp.TEST_IVP.y0
+        assert ivp.ODEFunction is robertson_ivp.initial_value_problem
+        assert not ivp.has_analytical_solution
+        assert ivp.t0 == pytest.approx(robertson_ivp.TEST_IVP.t0)
+
+    # Random string
+    fail_args_1 = ["fgfdgdfg"]
+    # Value passed to "-us"
+    fail_args_2 = ["-us", "fgdfg"]
+    # Value passed to "--use-static"
+    fail_args_3 = ["--use-static", "dfgdg"]
+
+    @pytest.mark.parametrize("args", [fail_args_1, fail_args_2, fail_args_3])
+    def test_parse_args_fail(self, args):
+        """Tests that the specified command line arguments cause robertson_integration.parse_args to exit the program."""
+
+        with pytest.raises(SystemExit):
+            robertson_integration.parse_args(args)
