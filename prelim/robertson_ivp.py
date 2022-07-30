84ce8f829815795284a796fd6ae974bd34e64093
= <17607121+Mou141@users.noreply.github.com>
2022-07-30 15:59:38 +0100

    Added Safer Random Initial Values Generator Function

--- prelim/robertson_ivp.py
+++ prelim/robertson_ivp.py
@@ -40,9 +40,25 @@ def get_random_y0():
     return y0
 
 
-def get_robertson_ivp():
-    """Returns an instance of stiff_functions.IVPTuple for the Robertson Chemical Kinetics IVP with randomly chosen y0 such that sum(y0) == 1.0."""
+def get_safe_random_y0():
+    """Returns a y0 array with the same conditions as get_random_y0, but y1 is guaranteed to not be much larger than y2 and y3."""
+    rng = np.random.default_rng()
 
-    y0 = get_random_y0()
+    # Two random numbers uniformly chosen between 0.25 and 0.5 and the third is what's left to make sum(y0) == 1.0
+    y01 = rng.uniform(0.25, 0.5)
+    y02 = rng.uniform(0.25, 0.5)
+    y03 = 1.0 - (y01 + y02)
+
+    y0 = np.array([y01, y02, y03])
+
+    rng.shuffle(y0)  # Assign random numbers to y0 indices at random
+
+    return y0
+
+
+def get_robertson_ivp(gen_func=get_random_y0):
+    """Returns an instance of stiff_functions.IVPTuple for the Robertson Chemical Kinetics IVP with randomly chosen y0 provided by gen_func (defaults to get_random_y0)."""
+
+    y0 = gen_func()
 
     return stiff_functions.IVPTuple(initial_value_problem, None, y0=y0, t0=0.0)
