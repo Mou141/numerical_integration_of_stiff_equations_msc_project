102b0dac9e46dda5cbb9b6e122b5d9d373876599
= <17607121+Mou141@users.noreply.github.com>
2022-09-29 20:59:11 +0100

    Test With Autonomous Function

---
+++ exp4_testing/initial_version_autonomous_test.py
@@ -0,0 +1,13 @@
+"""Tests the initial implementation of integrators.exp4.EXP4 with the same IVP as in initial_version_test.py
+but the differential equation has an autonomous formulation (it depends only on y)."""
+
+from initial_version_test import test_solution, TEST_T0, TEST_Y0, main
+
+
+def autonomous_test_function(t, y):
+    """Autonomous version of initial_version_test.test_function."""
+    return 2.0 * y
+
+
+if __name__ == "__main__":
+    main(TEST_T0, 10.0, 0.01, TEST_Y0, autonomous_test_function, test_solution)
