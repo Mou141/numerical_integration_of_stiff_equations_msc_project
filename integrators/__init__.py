81f093d1519895b69664707263e8bb5061f7821f
= <17607121+Mou141@users.noreply.github.com>
2022-09-30 00:11:19 +0100

    Changed __init__.py to Expose EXP4 Class Only By Default

--- integrators/__init__.py
+++ integrators/__init__.py
@@ -1,2 +1,5 @@
-# If imported with *, only import exp4. Not _common
-__all__ = ["exp4"]
+# When package imported with "from integrators import *" import EXP4 class only
+__all__ = ["EXP4"]
+
+# Allow EXP4 class to be imported directly at the package level
+from .exp4 import EXP4
