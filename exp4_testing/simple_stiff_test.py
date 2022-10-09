190b63991d68468ef729ae56e625e562610b5b36
= <17607121+Mou141@users.noreply.github.com>
2022-10-09 23:33:43 +0100

    Created simple_stiff_test.py

---
+++ exp4_testing/simple_stiff_test.py
@@ -0,0 +1,43 @@
+"""Tests the exp4 implementation with the simple 1D stiff IVP in stiff_functions.STIFF_IVP."""
+
+from pathlib import Path
+import sys
+
+# Path to parent folder of folder this file is in
+PARENT_PATH = Path(__file__).resolve().parent.parent
+
+# Path to prelim folder
+PRELIM_PATH = PARENT_PATH / "prelim"
+
+sys.path.extend([str(PARENT_PATH), str(PRELIM_PATH)])
+
+from integrators import exp4
+from stiff_functions import STIFF_IVP
+
+import matplotlib.pyplot as plt
+from scipy.integrate import solve_ivp
+
+
+def main():
+    t_span = (STIFF_IVP.t0, 10.0)
+
+    print("Integrating...")
+    results = solve_ivp(
+        STIFF_IVP.ODEFunction, t_span, STIFF_IVP.y0, method=exp4.EXP4, autonomous=True
+    )
+
+    if not results.success:
+        print("Integration Failed: '{0}'.".format(results.message))
+        sys.exit(1)
+
+    print("Integration Successful.")
+    print("\tnjev: {0}".format(results.njev))
+    print("\tnfev: {0}".format(results.nfev))
+    print("\tnpoints: {0}".format(len(results.t)))
+
+    y_exact = STIFF_IVP.SolutionFunction(results.t)
+
+    plt.plot(results.t, results.y[0], "b0", label="Numerical Solution")
+    plt.plot(results.t, y_exact, label="Analytical Solution")
+
+    plt.show()
